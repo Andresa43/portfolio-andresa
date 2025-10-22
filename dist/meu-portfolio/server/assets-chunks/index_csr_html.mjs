15122f9aafe738bd1e76be3fd6cca4a622e657df
Andresa <andresa.borges@acbprocessamento.com.br>
2025-10-22 09:47:35 -0300

    add dist

---
+++ dist/meu-portfolio/server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,19 @@
+export default `<!doctype html>
+<html lang="pt-br" data-beasties-container="">
+
+<head>
+  <meta charset="utf-8">
+  <title>MeuPortfolio</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <style>@font-face{font-family:'DM Sans';font-style:italic;font-weight:100 1000;font-display:swap;src:url(https://fonts.gstatic.com/s/dmsans/v17/rP2Fp2ywxg089UriCZa4ET-DNl0.woff2) format('woff2');unicode-range:U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face{font-family:'DM Sans';font-style:italic;font-weight:100 1000;font-display:swap;src:url(https://fonts.gstatic.com/s/dmsans/v17/rP2Fp2ywxg089UriCZa4Hz-D.woff2) format('woff2');unicode-range:U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}@font-face{font-family:'DM Sans';font-style:normal;font-weight:100 1000;font-display:swap;src:url(https://fonts.gstatic.com/s/dmsans/v17/rP2Hp2ywxg089UriCZ2IHSeH.woff2) format('woff2');unicode-range:U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face{font-family:'DM Sans';font-style:normal;font-weight:100 1000;font-display:swap;src:url(https://fonts.gstatic.com/s/dmsans/v17/rP2Hp2ywxg089UriCZOIHQ.woff2) format('woff2');unicode-range:U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}</style>
+<style>*{margin:0;padding:0;box-sizing:border-box}:root{color-scheme:light dark;--text-color:#392014;--text-color-acizentado:#9a7f6f;--vermelho:#c82917;--text-color-laranja-acizentado:#ccbcb4;--text-color-white:#FFFFFF;--background-color:#ca815b;--font-family-DM-Sans:"DM Sans", sans-serif}</style><link rel="stylesheet" href="styles-I3OUDI4E.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-I3OUDI4E.css"></noscript></head>
+
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-2CNVOAL2.js" type="module"></script></body>
+
+</html>`;
