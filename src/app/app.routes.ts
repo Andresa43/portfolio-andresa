3e226d5fbc17e9b7d17a634bab230007e21abc57
Andresa <andresa.borges@acbprocessamento.com.br>
2025-10-27 16:04:55 -0300

    fix route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { Downloads } from './pages/downloads/downloads';
 export const routes: Routes = [
     { path: '', pathMatch: 'full', title: 'Portfólio Andresa Fernandes', component: Info },
     { path: 'contato', title: 'contato', component: Contact },
-    { path: 'sobre', title: 'contato', component: About },
+    { path: 'sobre', title: 'sobre', component: About },
     { path: 'poesias', title: 'poesias', component: PoetryCanvas },
     { path: 'downloads', title: 'downloads', component: Downloads },
 ];
